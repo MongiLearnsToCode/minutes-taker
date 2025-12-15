a0f2928a4323c3113c58c9ee9766678ad9438aec
MongiLearnsToCode <sandiledlmn@gmail.com>
2025-12-15 22:32:49 +0200

    fix: trust proxies for HTTPS behind Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+        
+        // Trust Railway's proxy for HTTPS
+        $middleware->trustProxies(at: '*');
 
         $middleware->web(append: [
             HandleAppearance::class,
